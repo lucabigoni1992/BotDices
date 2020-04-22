adfadcc044dc9084eb950d90fe29d895b69ec32a
luca <lucabig92@live.it>
2020-04-22 14:28:24 +0200

    start bot

---
+++ bot.js
@@ -0,0 +1,4 @@
+ console.log("Application start at ... " + Date(Date.now()).toString())  ;
+const  discord=require('discord.js');
+const client=new discord.Client();
+client.login("NzAyNDM5MjAzMDM1Njc2NzEz.XqAkCQ.O88-JKo27PXbhxmpaY7O45kTa0g");
